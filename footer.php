de7b4575893b52de3096e53a1259204698ef578d
Nononosty <130338296+Nononosty@users.noreply.github.com>
2024-06-16 09:54:58 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div>
+        <div class="footer_text">
+            <p>Контакты:</p>
+            <p>+7(499)XXX-XX-XX</p>
+            <p>г. Москва, ул. Большая Семеновская XX</p>
+        </div>
+    </div>
+</footer>
---
+++ main.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magazin</title>
+    <?php  include_once 'link.php' ?>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<main class="main">
+    <div class="container">
+        <article class="article_1">
+            <div class="text2" >
+                <h1> Welcome </h1>
+                <button class="ooo" >
+                    <a class="textt" href="order.php">Оформить заказ</a>
+                </button>
+            </div>
+            <div class="image">
+                <img src="img/122.png" alt="picture_1" class="intro_img">
+            </div>
+        </article>
+    </div>
+
+    <div class="container">
+        <section class="sect">
+            <div class="sect_c">
+                <div class="hoody" data-description="Срок изготовления до 12 рабочих дней;
+Ткань: Футер трехниточный пенье (наивысшее качество);
+Состав: 80% Хлопок, 20% Полиэстер;
+">
+                    <img class="h_i" src="img/07.jpg">
+                    <h3>Baku oversize</h3>
+                    <button type="button" class="h_b"><a class="textt" href="order.php">Заказать</a></button>
+                </div> 
+                <div class="hoody" data-description="Срок изготовления до 12 рабочих дней;
+Ткань: Футер трехниточный пенье (наивысшее качество);
+Состав: 80% Хлопок, 20% Полиэстер;
+">
+                    <img class="h_i" src="img/02.jpg">
+                    <h3>Hine oversize</h3>
+                    <button type="button" class="h_b"><a class="textt" href="order.php">Заказать</a></button>
+                </div>
+                <div class="hoody" data-description="Срок изготовления до 12 рабочих дней;
+Ткань: Футер трехниточный пенье (наивысшее качество);
+Состав: 80% Хлопок, 20% Полиэстер;
+">
+                    <img class="h_i" src="img/03.jpg">
+                    <h3>Killu oversize</h3>
+                    <button type="button" class="h_b"><a class="textt" href="order.php">Заказать</a></button>
+                </div>
+                <div class="hoody" data-description="Срок изготовления до 12 рабочих дней;
+Ткань: Футер трехниточный пенье (наивысшее качество);
+Состав: 80% Хлопок, 20% Полиэстер;
+">
+                    <img class="h_i" src="img/07.jpg">
+                    <h3>Dekat oversize</h3>
+                    <button type="button" class="h_b"><a class="textt" href="order.php">Заказать</a></button>
+                </div>
+            </div>
+        </section>
+    </div>
+
+</main>
+
+<script src="cards.js"></script>
+
+
+
+</body>
